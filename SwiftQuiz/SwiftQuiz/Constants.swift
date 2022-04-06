0daeb1141a7c5e468677f191e565c31b119b12e0
alban <lacou.alban@gmail.com>
2022-04-06 16:48:17 +0200

    ajout des fonctions de call a l'api

---
+++ SwiftQuiz/SwiftQuiz/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  SwiftQuiz
+//
+//  Created by Alban on 05/04/2022.
+//
+
+import Foundation
+
+class Constants: NSObject {
+    
+    static let apiKey: String = "3e6c9bd7c42ef6acd7c8c9660e67bcf4"
+    static let apiBaseURL: String = "https://api.openweathermap.org/data/2.5/weather"
+}
--- SwiftQuiz/SwiftQuiz/ViewController.swift
+++ SwiftQuiz/SwiftQuiz/ViewController.swift
@@ -7,35 +7,79 @@
 
 import UIKit
 
-// Location: CoreLocation
-// custom cell: collection view
-// API / request to get the data
+class ViewController: UIViewController {
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
 
-    @IBOutlet var table: UITableView!
-    
-    var models = [Weather]()
+    @IBOutlet weak var weatherImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //self.getQuote()
         
-        // Register 2 cells
-        
-        table.delegate = self
-        table.dataSource = self
+        getInfosApi()
     }
-
-    // Table $
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return models.count
+    
+    func getInfosApi(){
+        
+        let lat: Int = 48
+        let lon: Int = 2
+        
+        let quoteUrl = URL(string: "\(Constants.apiBaseURL)?q=Paris&appid=\(Constants.apiKey)" )!
+        var request = URLRequest(url: quoteUrl)
+        request.httpMethod = "GET"
+        
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let data = data, error == nil {
+                
+                if let response = response as? HTTPURLResponse, response.statusCode == 200 {
+                    if let json = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers){
+                        
+                        if let data = json as? [String: AnyObject]{
+                            let main = data["main"]
+                            let tempEnKelvin = main!["temp"]!! as? Double
+                            let tempEnDegrees = Int(tempEnKelvin!) - 273
+                            print(tempEnDegrees)
+                            
+                            
+                            if let weather = data["weather"] as? [[String : AnyObject]]{
+                                print(weather[0]["icon"]!)
+                                print("https://openweathermap.org/img/w/\(weather[0]["icon"]!).png")
+                                
+                                self.weatherImageView.load(url: URL(string: "https://openweathermap.org/img/w/\(weather[0]["icon"]!).png")!)
+                            }
+                            
+                            
+                            
+                        }
+                        
+                    }
+                    
+                }
+            }
+            
+        }
+            
+        task.resume()
+        
     }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell()
+    
+    
+
 
 }
 
-struct Weather {
-    
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
 }
-
