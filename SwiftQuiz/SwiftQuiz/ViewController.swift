123e9437b49d99725f4242438a68cd9d36aed076
alban <lacou.alban@gmail.com>
2022-04-07 11:36:31 +0200

    merge

--- SwiftQuiz/SwiftQuiz/ViewController.swift
+++ SwiftQuiz/SwiftQuiz/ViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 import CoreLocation
 
-class ViewController: UIViewController {
-
-
-    @IBOutlet weak var weatherImageView: UIImageView!
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate  {
+    
+    
+    @IBOutlet var table: UITableView!
+    var models = [Weather]()
     
     let locationManager = CLLocationManager()
     
@@ -19,11 +20,53 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //self.getQuote()
         
-        getInfosApi()
+        // Register 2 cells
+      
+        table.register(WeatherTableViewCell.nib(), forCellReuseIdentifier: WeatherTableViewCell.identifier)
+        
+        table.delegate = self
+        table.dataSource = self
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setupLocation()
+    }
+    
+    func setupLocation() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if !locations.isEmpty, currentLocation == nil {
+            currentLocation = locations.first
+            locationManager.stopUpdatingLocation()
+            requestWeatherForLocation()
+        }
+    }
+    
+    func requestWeatherForLocation() {
+        guard let currentLocation = currentLocation else {
+            return
+        }
+        
+        let long = currentLocation.coordinate.longitude
+        let lat = currentLocation.coordinate.latitude
+        
+        print("\(long) | \(lat)")
+    }
+    
+    // Table
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
     
     func getInfosApi(){
         
@@ -52,28 +95,30 @@ class ViewController: UIViewController {
                                 print(weather[0]["icon"]!)
                                 print("https://openweathermap.org/img/w/\(weather[0]["icon"]!).png")
                                 
-                                self.weatherImageView.load(url: URL(string: "https://openweathermap.org/img/w/\(weather[0]["icon"]!).png")!)
+                                /*
+                                 self.weatherImageView.load(url: URL(string: "https://openweathermap.org/img/w/\(weather[0]["icon"]!).png")!)
+                                 }*/
+                                
+                                
+                                
                             }
-                            
-                            
                             
                         }
                         
                     }
-                    
                 }
+                
             }
             
-        }
             
+            
+        }
+        
         task.resume()
         
-    }
-    
-    
+        
+    }}
 
-
-}
 
 extension UIImageView {
     func load(url: URL) {
@@ -87,4 +132,8 @@ extension UIImageView {
             }
         }
     }
+}
+
+struct Weather {
+    
 }
